ef2ab04c9580ea934cc17a5330c4e0eb3cc4d3c4
Christopher Koerber <christopher@koerber-bochum.de>
2017-12-20 17:50:20 +0100

    added new figures and beta parameter to scripts

--- tex/mercury_orbit_base.py
+++ tex/mercury_orbit_base.py
@@ -6,7 +6,8 @@ rM0 = 4.6    # in units of R0
 vM0 = 0.51   # in units of R0/T0
 c_a = 1.01   # in units of R0/T0**2
 TM  = 88.    # in units of T0 
-rS  = 3.e-7  # in units of R0
+rS  = 3.0e-7 # in units of R0
+a2  = 8.2e-7 # in units of R0**2
 
 # Initialize distance and velocity vectors
 vec_rM0 = vector(0, rM0, 0)
@@ -16,8 +17,9 @@ vec_vM0 = vector(vM0, 0, 0)
 dt = 2 * vM0 / c_a / 20
 
 # Define the coordinate and velocity update
-def evolve_mercury(vec_rM_old, vec_vM_old, alpha):
-    aMS = c_a * ( 1 + alpha * rS / vec_rM_old.mag  ) / vec_rM_old.mag**2
+def evolve_mercury(vec_rM_old, vec_vM_old, alpha, beta):
+    temp = 1 + alpha * rS / vec_rM_old.mag + beta * a2 / vec_rM_old.mag**2
+    aMS  = c_a * temp / vec_rM_old.mag**2
     vec_aMS = - aMS * ( vec_rM_old / vec_rM_old.mag )
     vec_vM_new = vec_vM_old + vec_aMS * dt
     vec_rM_new = vec_rM_old + vec_vM_new * dt
@@ -32,10 +34,11 @@ S.velocity = vector(0,0,0)
 M.trajectory = curve(color=color.white)
 
 # Start the simulation
-t=0
-alpha = 0
+t     = 0.0
+alpha = 0.0
+beta  = 0.0
 while t < 2*TM:
     rate(100)
     M.trajectory.append(pos=M.pos)
-    M.pos , M.velocity = evolve_mercury(M.pos , M.velocity , alpha)
+    M.pos , M.velocity = evolve_mercury(M.pos , M.velocity , alpha, beta)
     t = t + dt
